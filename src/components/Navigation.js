4ab615817a716a355a41f091e00fcfeb550c24f2
michael-albrecht-1 <albrecht.michael@hotmail.fr>
2021-03-08 16:54:35 +0100

    nav bar

---
+++ src/components/Navigation.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import {NavLink } from 'react-router-dom';
+
+const Navigation = () => {
+    return (
+        <div className="sidebar">
+            <div className="id">
+                <div className="idContent">
+                    <img src="./media/billGates.jpg" alt="profile-pic"/>
+                    <h3>Michaël Albrecht</h3>
+                </div>
+            </div>
+
+            <div className="navigation">
+                <ul>
+                    <li>
+                        <NavLink exact to="/" activeClassName="navActive">
+                            <i className="fas fa-home"></i>
+                            <span>Accueil</span>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink exact to="/competences" activeClassName="navActive">
+                            <i className="fas fa-mountain"></i>
+                            <span>Compétences</span>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink exact to="/portfolio" activeClassName="navActive">
+                            <i className="fas fa-images"></i>
+                            <span>Portfolio</span>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink exact to="/contact" activeClassName="navActive">
+                            <i className="fas fa-address-book"></i>
+                            <span>Contact</span>
+                        </NavLink>
+                    </li>
+                </ul>
+            </div>
+
+            <div className="socialNetwork">
+                <ul>
+                    <li>
+                        <a href="#" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin"></i></a>
+                    </li>
+                    <li>
+                        <a href="#" target="_blank" rel="noopener noreferrer"><i className="fab fa-github"></i></a>
+                    </li>
+                </ul>
+                <div className="signature">
+                    <p>M.Albrecht</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navigation;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Navigation from '../components/Navigation';
+
 
 const Home = () => {
     return (
         <div>
-            home page
+            <Navigation />
+            Home sweet home
         </div>
     );
 };
